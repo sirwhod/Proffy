4727698520bbbfb2050d5359fdd1ee0f499e88dc
Rodrigo Ferreira Brandão <rbrandao@farma.local>
2020-08-04 17:48:02 -0300

    Commit extra Aula 1

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -11,15 +11,15 @@ function TeacherItem() {
                         <img src="https://avatars2.githubusercontent.com/u/61847070?s=460&u=a33de9e71b6abbd50cde5332aa0d2686bcd0a458&v=4" alt="Rodrigo Brandão"/>
                         <div>
                             <strong>Rodrigo Brandão</strong>
-                            <span>Química</span>
+                            <span>Front-end</span>
                         </div>
                     </header>
 
                     <p>
-                        Entusiasta das melhores tecnologias de química avançada.
+                        Entusiasta das melhores tecnologias de Programação Front-end.
                         <br /> <br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-                        Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                        Apaixonado por design e desenhos dono da marca de roupas lowRoad,
+                        Me identifico com tudo que é do meio artístico visual.
                     </p>
 
                     <footer>
